4d3354fc3136eea2c03a0aeb46cceb73c83d1f27
cyrus-lsl <cyrusleesl311@gmail.com>
2025-04-29 00:24:28 +0800

    Add files via upload

---
+++ tic-tc-toe/grid.h
@@ -0,0 +1,9 @@
+#ifndef GRID_H
+#define GRID_H
+
+// Function prototypes
+void printGrid(int grid);
+bool isFilled(int grid, int pos);
+bool updateGrid(int &grid, int pos, char mark);
+
+#endif // GRID_H
