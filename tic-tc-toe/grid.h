368787d04ac21ebb6dd43e141af4e34eb82dd5cd
cyrus-lsl <cyrusleesl311@gmail.com>
2025-04-29 00:27:27 +0800

    Delete tic-tc-toe directory

--- tic-tc-toe/grid.h
+++
@@ -1,9 +0,0 @@
-#ifndef GRID_H
-#define GRID_H
-
-// Function prototypes
-void printGrid(int grid);
-bool isFilled(int grid, int pos);
-bool updateGrid(int &grid, int pos, char mark);
-
-#endif // GRID_H
